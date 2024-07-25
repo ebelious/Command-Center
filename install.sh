fc8c22df98ad97acafda0ba77191937d23c20393
ebelious <belanger.eric.m@gmail.com>
2024-07-24 21:03:53 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -102,27 +102,75 @@ elif [[ $OS = PRETTY_Ubuntu ]] ||[[ $OS = PRETTY_Debian ]] || [[ $OS = Ubuntu ]]
 then
     sudo apt install figlet htop lm-sensors htop vim grc dmidecode docker.io git lf fzf bat curl hashid hashcat john nmap ninvaders sipcalc npm cargo python2.7 apropos lsd 
     clear
+# Adding comment to bash and zsh for added aliases
+    cd ~/Command-Center/
+    echo "# These are the Command-Center aliases" >> $HOME/.bashrc
+    echo "# These are the Command-Center aliases" >> $HOME/.zshrc
 # Getting lazy docker git repo
     echo -e "\e[1;32mInstalling lazydocker...\e[0m"
     curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
+    sleep 3
     clear
 # Getting metasploit
     echo -e "\e[1;32mInstalling metasploit...\e[0m"
     curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
     chmod 755 msfinstall && \
     ./msfinstall
+    sleep 3
     clear
 # Getting sqlmap
     echo -e "\e[1;32mInstalling sqlmap...\e[0m"
     git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
-    echo "alias sqlmap='python ~/Command-Center/sqlmap-dev/sqlmap.py' "
+    echo "alias sqlmap='python ~/Command-Center/sqlmap-dev/sqlmap.py' " >> $HOME/.bashrc
+    echo "alias sqlmap='python ~/Command-Center/sqlmap-dev/sqlmap.py' " >> $HOME/.zshrc
     clear
 # Getting recon-ng
+    echo -e "\e[1;32mInstalling recon-ng...\e[0m"
     git clone https://github.com/lanmaster53/recon-ng.git
     cd recon-ng
     sudo docker build --rm -t recon-ng .
-    echo "alias ccenter='~/Command-Center/command-center.sh'" >> ~/.bashrc
-    echo "alias ccenter='~/Command-Center/command-center.sh'" >> ~/.zshrc
+    echo "alias ccenter='~/Command-Center/command-center.sh'" >> $HOME/.bashrc
+    echo "alias ccenter='~/Command-Center/command-center.sh'" >> $HOME/.zshrc
+    clear
+# Installing Seclists
+    echo -e "\e[1;32mInstalling seclists...\e[0m"
+    git clone https://github.com/danielmiessler/SecLists.git
+    clear
+# Installing Zellij
+    echo -e "\e[1;32mInstalling zellij...\e[0m"
+    cd ~/Command-Center/
+    wget https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz
+    tar -xf zellij-x86_64-unknown-linux-musl.tar.gz
+    cp zellij ~/.local/bin/
+    clear
+    # Install Virust Total CLI
+    clear
+    echo -e "\e[1;32mInstalling virustotal-cli...\e[0m"
+    cd ~/Command-Center/
+    git clone https://github.com/VirusTotal/vt-cli.git
+    cd vt-cli
+    make install
+    export GOBIN=`go env GOPATH`/bin
+    export PATH=$PATH:$GOBIN
+    clear
+# Install oui lookup
+    cd ~/Command-Center/
+    echo -e "\e[1;32mInstalling npm oui...\e[0m"
+    sudo npm i -g oui
+    clear
+    # Installing Responder
+    echo -e "\e[1;32mInstalling responder...\e[0m"
+    git clone https://github.com/SpiderLabs/Responder.git
+    echo "alias responder='python2.7 ~/Command-Center/Responder/Responder.py'" >> $HOME/.bashrc
+    echo "alias responder='python2.7 ~/Command-Center/Responder/Responder.py'" >> $HOME/.zshrc
+    clear
+# Installing Ttype
+    echo -e "\e[1;32mInstalling ttyper...\e[0m"
+    cd ~/Command-Center/
+    cargo install ttyper
+    echo "alias ttyper='$HOME/.cargo/bin/ttyper'" >> $HOME/.bashrc
+    echo "alias ttyper='$HOME/.cargo/bin/ttyper'" >> $HOME/.zshrc
+    clear
     ~/Command-Center/command-center.sh
 #
 #
